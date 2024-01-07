3557b7f99a2ef792c4f0b1615f2f1cc9a1b9817d
Manik Sharma <maniksharma@Maniks-MacBook-Air.local>
2024-01-07 20:58:34 +0530

    backend updated

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const { router } = require("./routes/index") 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 console.log(PORT)
 const app = express()
 
